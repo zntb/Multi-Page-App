7a3a7a343674201c16010407b528ad5df23572b2
zntb <zenetibi@yahoo.com>
2023-12-28 09:07:31 +0200

    Navigating between Pages with Links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  Route,
+  // Route,
 } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Products from './pages/Products';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <Home /> },
-//   { path: '/products', element: <Products /> },
-// ]);
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
+]);
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>.
+      </p>
+    </>
+  );
 }
 export default Home;
